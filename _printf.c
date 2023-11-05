3548943e9b862a3d429ab17894c50e5c0db2c398
Cryptonfused <cryptonfused@gmail.com>
2023-11-05 14:16:23 -0800

    new mods to new v of printf funct

--- _printf.c
+++ _printf.c
@@ -1,7 +1,8 @@
 #include "main.h"
-#include <stdarg.h>
 #include <unistd.h>
-
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
 /**
  * _printf - Custom printf function
  * @format: Format string
@@ -11,40 +12,54 @@
  */
 int _printf(const char *format, ...)
 {
-    int charCount = 0;
-    va_list args;
+	int charCount = 0;
+	va_list args;
 
-    if (!format || !format[0])
-        return (0);
+	if (!format || !format[0])
+		return (-1);
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            if (*format == '%')
-            {
-                write(1, format - 1, 1);
-                charCount++;
-            }
-            else
-            {
-                format--;
-                write(1, format, 2);
-                charCount += 2;
-                format++;
-            }
-        }
-        else
-        {
-            write(1, format, 1);
-            charCount++;
-        }
-        format++;
-    }
+	while (format && *format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					charCount += print_character(args);
+					break;
+				case 's':
+					{
+					const char *str = va_arg(args, const char *);
+                        		if (str == NULL)
+					{
+						charCount += write(1, "(null)", 6);
+					}else
+					{
+						charCount += write(1, str, strlen(str));
+					}
+					}
+					break;
+				case '%':
+					write(1, format, 1);
+					charCount++;
+					break;
+				default:
+					write(1, format - 1, 1);
+					write(1, format, 1);
+					charCount += 2;
+			}
+		}
+		else
+		{
+			write(1, format, 1);
+			charCount++;
+		}
+		format++;
+	}
 
-    va_end(args);
-    return (charCount);
+	va_end(args);
+	return (charCount);
 }
