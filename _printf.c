9150dde00cfee0bc47b854f368d97705a3f36496
Cryptonfused <cryptonfused@gmail.com>
2023-11-07 06:07:57 -0800

    mods to _printf funct

--- _printf.c
+++ _printf.c
@@ -1,52 +1,50 @@
 #include "main.h"
-
+#include <stdarg.h>
+#include <unistd.h>
+/**
+ * _printf - Custom printf function
+ * @format: The format string containing format specifiers
+ * @...: Variable number of arguments to be printed
+ *
+ * Return: The number of characters printed
+ */
 int _printf(const char *format, ...)
 {
-    int co = 0;
+	int count = 0;
+	va_list args;
 
-    va_list args;
-    va_start(args, format);
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            if (*format == 'c') /*print character*/
-            {
-                char cha = va_arg(args, int);
-                _putchar(cha);
-                co++;
-                format++;
-            }
-            else if (*format == 's') /*print argument string*/
-            {
-                char *str =  va_arg(args, char*);
-                co += _print_str(str);
-                format++;
-            }
-            else if (*format == '%') /*for %% print %*/
-            {
-                _putchar('%');
-                co++;
-                format++;
-            }
-            else /*%string or other character*/
-            {
-                if (*format)
-                {
-                    format--;
-                    _putchar(*format);
-                    format++;
-                }
-                co++;
-            }
-        }
-        else
-        {
-            _putchar(*format);
-            co++;
-            format++;
-        }
-    }
-    return (co);
+	va_start(args, format);
+	if (format == NULL)
+        	return (-1);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '\0')
+			{
+				count += _putchar('%');
+				break;
+			}
+			if (*format == 'c')
+				count += _putchar(va_arg(args, int));
+			else if (*format == 's')
+				count += _print_str(va_arg(args, char *));
+			else if (*format == '%')
+				count += _putchar('%');
+			else
+			{
+				count += _putchar(*(format - 1));
+				count += _putchar(*format);
+			}
+			format++;
+		}
+		else
+		{
+			count += _putchar(*format);
+			format++;
+		}
+	}
+	va_end(args);
+	return (count);
 }
--- main.c
+++ main.c
@@ -30,8 +30,8 @@ int main(void)
     printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
     _printf("Character:[%c]\n", 'H');
     printf("Character:[%c]\n", 'H');
-    _printf("String:[%s]\n", "I am a string !");
     printf("String:[%s]\n", "I am a string !");
+    _printf("String:[%s]\n", "I am a string !");
     _printf("Address:[%p]\n", addr);
     printf("Address:[%p]\n", addr);
     len = _printf("Percent:[%%]\n");
@@ -39,5 +39,17 @@ int main(void)
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
     _printf("Unknown:[%r]\n");
+    len = _printf("%\n");
+    len2 = printf("%\n");
+    printf("Length:[%d, %i]\n", len, len);
+    _printf("Length:[%d, %i]\n", len2, len2);
+    _printf("%JJ\n");
+    printf("%JJ\n");
+    _printf("%!\n");
+    _printf("%K\n");
+    _printf("String:[%s]\n", "I am a string !");
+    _printf("String:[%s]\n", NULL);
+    _printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);
+    _printf(NULL);
     return (0);
 }
