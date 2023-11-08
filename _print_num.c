1a1bdcbb45990253a3d8478ae3e80b3ae99a4f0d
Cryptonfused <cryptonfused@gmail.com>
2023-11-08 01:51:50 -0800

    final mods

--- _print_num.c
+++ _print_num.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <limits.h>
+
 /**
- * _print_num - Prints an integer to stdout
+ * _print_num - Print an integer to stdout
  * @n: The integer to be printed
  *
  * Return: The number of characters printed
@@ -9,36 +10,29 @@
 int _print_num(int n)
 {
 	int count = 0;
-	int divisor, digit;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		count++;
-		if (n == INT_MIN) /* Handle the case where n is most negative value) */
+		if (n == INT_MIN)
 		{
-			n++; /* Increment n to avoid overflow when negating */
-			n = -n;
-			_putchar('2'); /* Print the first digit */
-			count++;
+			n = -(n + 1);
 		}
 		else
 		{
 			n = -n;
 		}
-	}
-	divisor = 1; /* Initialize divisor with 10 */
-	while (n / divisor >= 10)
-	{
-		divisor *= 10;
-	}
-	while (divisor > 0)
-	{
-		digit = n / divisor;
-		_putchar(digit + '0');
 		count++;
-		n %= divisor;
-		divisor /= 10;
 	}
+
+	while (n / 10 > 0)
+	{
+		count += _print_num(n / 10);
+		n = n % 10;
+	}
+
+	_putchar(n + '0');
+	count++;
+
 	return (count);
 }
