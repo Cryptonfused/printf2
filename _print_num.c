2c804c04526641e6377475b98ef87b7155de5791
Cryptonfused <cryptonfused@gmail.com>
2023-11-08 00:23:13 -0800

    mod to _print_num funct to handle most neg and pos int

--- _print_num.c
+++ _print_num.c
@@ -1,24 +1,44 @@
 #include "main.h"
+#include <limits.h>
 /**
- * _print_num - Print an integer to stdout
+ * _print_num - Prints an integer to stdout
  * @n: The integer to be printed
  *
  * Return: The number of characters printed
  */
 int _print_num(int n)
 {
-    int count = 0;
+	int count = 0;
+	int divisor, digit;
 
-    if (n < 0)
-    {
-        n = -n;
-        _putchar('-');
-        count++;
-    }
-
-    if (n / 10 != 0)
-        count += _print_num(n / 10);
-    _putchar((n % 10) + '0');
-    count++;
-    return (count);
+	if (n < 0)
+	{
+		_putchar('-');
+		count++;
+		if (n == INT_MIN) /* Handle the case where n is most negative value) */
+		{
+			n++; /* Increment n to avoid overflow when negating */
+			n = -n;
+			_putchar('2'); /* Print the first digit */
+			count++;
+		}
+		else
+		{
+			n = -n;
+		}
+	}
+	divisor = 10; /* Initialize divisor with 10 */
+	while (n / divisor >= 10)
+	{
+		divisor *= 10;
+	}
+	while (divisor > 0)
+	{
+		digit = n / divisor;
+		_putchar(digit + '0');
+		count++;
+		n %= divisor;
+		divisor /= 10;
+	}
+	return (count);
 }
